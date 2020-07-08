727f954f87e80b977b16054d891b03a9ac3f6a57
João Pedro Giarrante <joao.giarrante@valemobi.com.br>
2020-07-08 01:49:08 -0300

    first / last commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func btnPressed(_ sender: UIButton) {
+        playSound(soundID: sender.titleLabel?.text ?? "nil")
+        delayAlpha(sender: sender)
+    }
+    
+    func delayAlpha(sender:UIButton) {
+        //Reduces the sender's (the button that got pressed) opacity to half.
+        sender.alpha = 0.5
+        //Code should execute after 0.2 second delay.
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+        //Bring's sender's opacity back up to fully opaque.
+        sender.alpha = 1.0
+        }
+    }
 
+    func playSound(soundID: String) {
+        if(soundID=="nil"){return}
+        let url = Bundle.main.url(forResource: soundID, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
     
 
 }
